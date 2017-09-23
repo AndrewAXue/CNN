20ce7cdd6b643217bbe4cf37901979df2840fbfa
fishlicker <a3xue@edu.uwaterloo.ca>
2017-09-23 02:13:10 -0400

    added graphics for debugging, confirmed feed forward works for pooling and convolutions, back propagation rewritten for y-dev and x-dev still needs testing

--- src/cnn/cnn.java
+++ src/cnn/cnn.java
@@ -1,10 +1,28 @@
 package cnn;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+
 public class cnn {
+	
+	JFrame window = new JFrame("VISUALIZATION");
+	JFrame weights_window = new JFrame("VISUALIZATION V2");
+	boolean draw = true;
+	DecimalFormat round = new DecimalFormat("#.##");
 	
 static Scanner scanner;
 	
@@ -59,15 +77,15 @@ static Scanner scanner;
 	
 	
 	
+	boolean debug = true;
 	
 	
 	
 	
-	
-	int conv_stride=2;
+	int conv_stride=1;
 	int conv_pad=1;
 	
-	int pool_stride=1;
+	int pool_stride=2;
 	
 	int data_depths[];
 	int data_width_heights[];
@@ -95,13 +113,10 @@ static Scanner scanner;
 	}
 	
 	public class cnn_nodeclass{
-		double y_value,x_value;
+		double y_value=0;
+		double x_value=0;
 		double y_dev=0;
 		double x_dev=0;
-		cnn_nodeclass(){
-			y_value = valuechoose.nextDouble();
-			x_value = valuechoose.nextDouble();
-		}
 	}
 	// data[layer][depth][height][width];
 	cnn_nodeclass data [][][][];
@@ -114,13 +129,14 @@ static Scanner scanner;
 		// For pooling, this is the size of the pooling matrix
 		int depth;
 		int matrix_width_height;
-		// matrix_values[depth][height][width]
+		// matrix_values[depth][numperdepth = data_depths[i]][height][width]
 		weightclass matrix_values[][][][];
 		double bias;
 		int stride;
 		int zero_padding;
 		// only "max"
 		String pooling_type;
+		
 		cnn_layerclass(String temp_function,int temp_depth,int temp_matrix_width_height,
 						weightclass temp_matrix_values[][][][],double temp_bias,
 						int temp_stride,
@@ -177,6 +193,8 @@ static Scanner scanner;
 	//cnn_layerclass two_pool;
 	
 	cnn(int temp_data_depths[],int temp_data_width_heights[],String temp_layer_types[],int temp_layer_depths[],int temp_layer_width_heights[]){
+		if (draw) create_window();
+		
 		data_depths = new int[temp_data_depths.length];
 		data_width_heights = new int[temp_data_width_heights.length];
 		layer_depths = new int[temp_layer_depths.length];
@@ -234,48 +252,52 @@ static Scanner scanner;
 				cnn_layers[i] = new cnn_layerclass("conv",temp_layer_depths[i],temp_layer_width_heights[i],
 						temp_matrix_values,0,conv_stride,conv_pad,"");
 				cnn_layerclass cur = cnn_layers[i];
-				
-				if (i==0){
-					double test_weights[][][][] = {
-							{
-							{{1,1,0},{0,0,-1},{0,-1,-1}},
-							{{0,0,0},{1,0,0},{0,1,-1}},
-							{{1,-1,-1},{1,-1,0},{1,0,0}}},
-							
-							{{{0,1,0},{-1,0,-1},{1,0,0}},
-							{{0,1,-1},{1,1,0},{0,-1,0}},
-							{{1,1,1},{0,-1,-1},{1,0,-1}}}
-							};
-							
-					for (int z=0;z<2;z++){
-						for (int a=0;a<3;a++){
-							for (int b=0;b<3;b++){
-								for (int c=0;c<3;c++){
-									//System.out.println(z+" "+a+" "+b+" "+c);
-									cur.matrix_values[z][a][b][c].weight = test_weights[z][a][b][c];
+				if (debug){
+					System.out.println("HI");
+					if (i==0){
+						double test_weights[][][][] = {
+								{
+								{{0,0,0},{0,1,0},{0,1,-1}},
+								{{1,0,0},{0,-1,-1},{0,1,-1}},
+								{{1,1,0},{1,-1,-1},{-1,1,0}}},
+								
+								{{{1,1,-1},{1,1,-1},{-1,1,1}},
+								{{0,-1,-1},{0,-1,1},{-1,1,0}},
+								{{-1,1,1},{-1,-1,0},{1,1,-1}}}
+								};
+								
+						for (int z=0;z<2;z++){
+							for (int a=0;a<3;a++){
+								for (int b=0;b<3;b++){
+									for (int c=0;c<3;c++){
+										//System.out.println(z+" "+a+" "+b+" "+c);
+										cur.matrix_values[z][a][b][c].weight = test_weights[z][a][b][c];
+									}
 								}
 							}
 						}
 					}
-				}
-				else{
-					double test_weights[][][][] = {
-							{
-							{{2,3},{-1,1}},
-							{{3,0},{1,2}}
-							}
-							};
-							
-					for (int z=0;z<1;z++){
-						for (int a=0;a<2;a++){
-							for (int b=0;b<2;b++){
-								for (int c=0;c<2;c++){
-									//System.out.println(z+" "+a+" "+b+" "+c);
-									cur.matrix_values[z][a][b][c].weight = test_weights[z][a][b][c];
+					
+					else{
+						double test_weights[][][][] = {
+								{
+								{{2,3,4},{-1,1,-2},{4,2,1}},
+								{{3,0,2},{1,2,0},{1,2,0}}
+								}
+								};
+								
+						for (int z=0;z<1;z++){
+							for (int a=0;a<2;a++){
+								for (int b=0;b<3;b++){
+									for (int c=0;c<3;c++){
+										//System.out.println(z+" "+a+" "+b+" "+c);
+										cur.matrix_values[z][a][b][c].weight = test_weights[z][a][b][c];
+									}
 								}
 							}
 						}
 					}
+					
 				}
 				
 				
@@ -309,6 +331,7 @@ static Scanner scanner;
 			for (int k=0;k<input_data[0].length;k++){
 				for (int z=0;z<input_data[0][0].length;z++){
 					data[0][i][k][z].y_value = input_data[i][k][z];
+					data[0][i][k][z].x_value = input_data[i][k][z];
 				}
 			}
 		}
@@ -348,7 +371,7 @@ static Scanner scanner;
 							sum+=layer_props.bias;
 							data[i+1][a][b][c].x_value = sum;
 							data[i+1][a][b][c].y_value = sum;
-							//data[i+1][a][b][c].y_value = sigmoid(sum);							
+							if (!debug) data[i+1][a][b][c].y_value = sigmoid(sum);							
 						}
 					}
 				}
@@ -427,6 +450,78 @@ static Scanner scanner;
 	}
 	
 	void cnn_back_propagate(double last_layer_error[]){
+		for (int a=0;a<data_width_heights[data_width_heights.length-1];a++){
+			for (int b=0;b<data_width_heights[data_width_heights.length-1];b++){
+				data[data.length-1][0][a][b].x_dev = last_layer_error[a*data_width_heights[data_width_heights.length-1]+b];
+				//data[data.length-1][0][a][b].y_dev = null;
+			}
+		}
+		/*
+		int data_depths[];
+		int data_width_heights[];
+		int layer_depths[];
+		int layer_width_heights[];
+		*/
+		// Only ydevs
+		for (int a=data_width_heights.length-1;a>=1;a--){
+			cnn_layerclass active_layer = cnn_layers[a-1];
+			if (cnn_layers[a-1].function.compareTo("conv")==0){
+				for (int b=0;b<data_depths[a];b++){
+					for (int c=0;c<data_width_heights[a];c++){
+						for (int d=0;d<data_width_heights[a];d++){
+							// Found correct active_dev
+							double active_dev = data[a][b][c][d].x_dev;
+							if (active_dev!=0){
+								int init_x = -active_layer.zero_padding+d*active_layer.stride;
+								int init_y = -active_layer.zero_padding+c*active_layer.stride;
+								int solved_x,solved_y;
+								for (int e=0;e<data_depths[a-1];e++){
+									for (int f=0;f<layer_width_heights[a-1];f++){
+										for (int g=0;g<layer_width_heights[a-1];g++){
+											solved_x = init_x+g;
+											solved_y = init_y+f;
+											if (solved_x>=0&&solved_x<data_width_heights[a-1]&&solved_y>=0&&solved_y<data_width_heights[a-1]){
+												data[a-1][e][solved_y][solved_x].y_dev+=active_dev*active_layer.matrix_values[b][e][f][g].weight;
+											}
+										}
+									}
+								}
+							}
+							
+						}
+					}
+				}
+				for (int b=0;b<data_depths[a-1];b++){
+					for (int c=0;c<data_width_heights[a-1];c++){
+						for (int d=0;d<data_width_heights[a-1];d++){
+							data[a-1][b][c][d].x_dev = data[a-1][b][c][d].y_dev*sigmoidprime(data[a-1][b][c][d].x_value);
+						}
+					}
+				}
+			}
+			else if (cnn_layers[a-1].function.compareTo("pool")==0){
+				for (int b=0;b<data_depths[a];b++){
+					for (int c=0;c<data_width_heights[a];c++){
+						for (int d=0;d<data_width_heights[a];d++){
+							double target = data[a][b][c][d].y_value;
+							
+							int init_x = d*active_layer.stride;
+							int init_y = c*active_layer.stride;
+							
+							for (int e=0;e<active_layer.stride;e++){
+								for (int f=0;f<active_layer.stride;f++){
+									if (target==data[a-1][b][init_y+e][init_x+f].y_value){
+										data[a-1][b][init_y+e][init_x+f].y_dev = data[a][b][c][d].y_dev;
+										data[a-1][b][init_y+e][init_x+f].x_dev = data[a][b][c][d].x_dev;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		/*
 		//NBP1
 		for (int a=0;a<data_width_heights[data_width_heights.length-1];a++){
 			for (int b=0;b<data_width_heights[data_width_heights.length-1];b++){
@@ -435,7 +530,7 @@ static Scanner scanner;
 			}
 		}
 		
-		for (int i=data_depths.length-2;i>=1;i--){
+		for (int i=data_depths.length-2;i>=0;i--){
 			cnn_layerclass layer_props = cnn_layers[i];
 			if (layer_props.function.compareTo("pool")==0){
 				for (int k=0;k<data_depths[i+1];k++){
@@ -467,10 +562,12 @@ static Scanner scanner;
 							for (int e=0;e<data_depths[i+1];e++){
 								for (int c=0;c<cnn_layers[i].matrix_width_height;c++){
 									for (int d=0;d<cnn_layers[i].matrix_width_height;d++){
-										if (a-c>=0&&b-d>=0)sum_y_dev+=data[i+1][e][a-c][b-d].x_dev*cnn_layers[i].matrix_values[e][k][c][d].weight;
+										//System.out.println((i+1)+" "+e+" "+(a-c)+" "+(b-d));
+										if (a-c>=0&&b-d>=0&&a-c<data[i+1][e].length&&b-d<data[i+1][e][a-c].length)sum_y_dev+=data[i+1][e][a-c][b-d].x_dev*cnn_layers[i].matrix_values[e][k][c][d].weight;
 									}
 								}
 							}
+							
 							active.y_dev += sum_y_dev;
 							active.x_dev += sum_y_dev*sigmoidprime(active.x_value);
 						}
@@ -503,7 +600,7 @@ static Scanner scanner;
 					}
 				}
 				*/
-				
+				/*
 				for (int k=0;k<layer_depths[i];k++){
 					//matrix_values[depth][num matrix per depth][height][width]
 					for (int a=0;a<data_depths[i+1];a++){
@@ -512,20 +609,47 @@ static Scanner scanner;
 								//System.out.println(k+" "+a+" "+b+" "+c);
 								weightclass active = cnn_layers[i].matrix_values[k][a][b][c];
 								double weight_sum = 0;
+								System.out.println(data_width_heights[i]-cnn_layers[i].matrix_width_height);
 								for (int d=0;d<=data_width_heights[i]-cnn_layers[i].matrix_width_height;d++){
 									for (int e=0;e<=data_width_heights[i]-cnn_layers[i].matrix_width_height;e++){
 										weight_sum+=data[i][k][d][e].x_dev*data[i-1][k][d+b][e+c].y_value;
 									}
 								}
+								System.out.println("weight dev "+weight_sum);
 								active.weightdev += weight_sum;
 							}
 						}
 					}
 					
 				}
+				/*supposed to be backwards
+				//System.out.println("conv "+i);
+				for (int z=0;z<cnn_layers[i].depth;z++){
+					for (int k=0;k<data_depths[i];k++){
+						for (int a=0;a<cnn_layers[i].matrix_width_height;a++){
+							for (int b=0;b<cnn_layers[i].matrix_width_height;b++){
+								weightclass active = cnn_layers[i].matrix_values[z][k][a][b];
+								double sum=0;
+								for (int d=0;d<data[i+1][0].length;d++){
+									for (int e=0;e<data[i+1][0].length;e++){
+										double xdev = data[i+1][z][d][e].x_dev;
+										int x_ind = -cnn_layers[i].zero_padding+a+d*cnn_layers[i].stride;
+										int y_ind = -cnn_layers[i].zero_padding+b+e*cnn_layers[i].stride;
+										if (x_ind>=0&&y_ind>=0&&x_ind<data[i][a].length&&y_ind<data[i][a].length){
+											//System.out.println(x_ind+" "+y_ind+" "+data[i][k][x_ind][y_ind].y_value);
+											sum+=xdev*data[i][k][x_ind][y_ind].y_value;
+										}
+									}
+								}
+								active.weightdev = sum;
+							}
+						}
+					}
+				}
 			}
 			
 		}
+*/
 	}
 	
 	void print_values(){
@@ -562,8 +686,8 @@ static Scanner scanner;
 		for (int i=0;i<cnn_layers.length;i++){
 			for (int z=0;z<cnn_layers[i].depth;z++){
 				for (int k=0;k<data_depths[i];k++){
-					for (int a=0;a<data_width_heights[i+1];a++){
-						for (int b=0;b<data_width_heights[i+1];b++){
+					for (int a=0;a<cnn_layers[i].matrix_width_height;a++){
+						for (int b=0;b<cnn_layers[i].matrix_width_height;b++){
 							if (cnn_layers[i].function.compareTo("conv")==0)System.out.print(cnn_layers[i].matrix_values[z][k][a][b].weight+"   ");
 						}
 						System.out.print('\n');
@@ -594,5 +718,161 @@ static Scanner scanner;
 			
 			System.out.println("layer done\n\n\n");
 		}
+	}
+	
+	protected void create_window(){
+		//Set characteristics of window
+		window = new JFrame("VISUALIZATION");
+		window.setSize(1000, 1000);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setIconImage(new ImageIcon("neural.jpg").getImage());
+		window.add(new VISUALIZATION());
+		window.addMouseListener(new mouseevent());
+		window.setVisible(true);
+		
+		weights_window = new JFrame("VISUALIZATION");
+		weights_window.setSize(1000, 1000);
+		weights_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		weights_window.setResizable(false);
+		weights_window.setIconImage(new ImageIcon("neural.jpg").getImage());
+		weights_window.add(new VISUALIZATION_WEIGHTS());
+		weights_window.addMouseListener(new mouseevent());
+		weights_window.setVisible(true);
+	}
+	
+	private class VISUALIZATION extends JComponent {
+			
+		VISUALIZATION() {
+            setPreferredSize(new Dimension(1000, 1000));
+        }
+		
+		public void paintComponent(Graphics g){
+			super.paintComponents(g);
+			Graphics2D grap = (Graphics2D) g; 	
+			window.setBackground(Color.WHITE);
+			
+			/*
+			 * int data_depths[];
+	int data_width_heights[];
+	int layer_depths[];
+	int layer_width_heights[];
+			 */
+			int num_width_height = 60;
+			grap.setColor(Color.BLACK);
+			
+			int rect_x_ind = 0;
+			int rect_y_ind = 0;
+			int draw_x_ind = 0;
+			int draw_y_ind = 0;
+			for (int i=0;i<data.length;i++){
+				//System.out.println("layer "+i);
+				for (int a=0;a<data[i].length;a++){
+					//System.out.println("Depth "+a);
+					//System.out.println(rect_x_ind+" "+rect_y_ind+" ");
+					grap.drawRect(rect_x_ind, rect_y_ind, data[i][0].length*num_width_height, data[i][0].length*num_width_height);
+					draw_x_ind = rect_x_ind;
+					draw_y_ind = rect_y_ind;
+					for (int b=0;b<data[i][0].length;b++){
+						for (int c=0;c<data[i][0].length;c++){
+							grap.drawRect(draw_x_ind, draw_y_ind, num_width_height, num_width_height);
+							grap.drawString("x "+Double.valueOf(round.format(data[i][a][b][c].x_value)), draw_x_ind+5, draw_y_ind+10);
+							grap.drawString("y "+Double.valueOf(round.format(data[i][a][b][c].y_value)), draw_x_ind+5, draw_y_ind+10+num_width_height/4);
+							grap.drawString("dx "+Double.valueOf(round.format(data[i][a][b][c].x_dev)), draw_x_ind+5, draw_y_ind+10+2*num_width_height/4);
+							grap.drawString("dy "+Double.valueOf(round.format(data[i][a][b][c].y_dev)), draw_x_ind+5, draw_y_ind+10+3*num_width_height/4);
+
+							draw_x_ind+=num_width_height;
+						}
+						draw_y_ind+=num_width_height;
+						draw_x_ind = rect_x_ind;
+					}
+					rect_y_ind+=data[i][0].length*num_width_height+20;
+				}
+				rect_x_ind+=data[i][0].length*num_width_height+20;
+				rect_y_ind = 0;
+			}	
+			//System.out.println("Done drawing");
+		}
+	}
+	
+	private class VISUALIZATION_WEIGHTS extends JComponent {
+		
+		VISUALIZATION_WEIGHTS() {
+            setPreferredSize(new Dimension(1000, 1000));
+        }
+		
+		public void paintComponent(Graphics g){
+			super.paintComponents(g);
+			Graphics2D grap = (Graphics2D) g; 	
+			window.setBackground(Color.WHITE);
+			
+			/*
+			 * int data_depths[];
+	int data_width_heights[];
+	int layer_depths[];
+	int layer_width_heights[];
+			 */
+			int num_width_height = 60;
+			grap.setColor(Color.BLACK);
+			
+			int rect_x_ind = 0;
+			int rect_y_ind = 0;
+			int draw_x_ind = 0;
+			int draw_y_ind = 0;
+			
+			// matrix_values[depth][numperdepth = data_depths[i]][height][width]
+			for (int i=0;i<cnn_layers.length;i++){
+				//System.out.println("layer "+i);
+				grap.setStroke(new BasicStroke(1));
+				if (cnn_layers[i].function.compareTo("conv")==0){
+					grap.drawRect(rect_x_ind, rect_y_ind, num_width_height*cnn_layers[i].matrix_width_height*cnn_layers[i].matrix_values.length, num_width_height*cnn_layers[i].matrix_width_height*cnn_layers[i].matrix_values[0].length);
+					draw_x_ind = rect_x_ind;
+					draw_y_ind = rect_y_ind;
+					for (int a=0;a<cnn_layers[i].matrix_values.length;a++){
+						weightclass cur_mat[][][][] = cnn_layers[i].matrix_values;
+						for (int b=0;b<cnn_layers[i].matrix_values[0].length;b++){
+							grap.setStroke(new BasicStroke(2));
+							grap.drawRect(draw_x_ind, draw_y_ind, num_width_height*cnn_layers[i].matrix_width_height, num_width_height*cnn_layers[i].matrix_width_height);
+							grap.setStroke(new BasicStroke(1));
+							int temp_draw_x_ind = draw_x_ind;
+							for (int c=0;c<cnn_layers[i].matrix_width_height;c++){
+								for (int d=0;d<cnn_layers[i].matrix_width_height;d++){
+									grap.drawRect(draw_x_ind, draw_y_ind, num_width_height, num_width_height);
+									grap.drawString("W "+Double.valueOf(round.format(cur_mat[a][b][c][d].weight)), draw_x_ind, draw_y_ind+20);
+									grap.drawString("dW "+Double.valueOf(round.format(cur_mat[a][b][c][d].weightdev)), draw_x_ind, draw_y_ind+40);
+									draw_x_ind+=num_width_height;
+								}
+								draw_x_ind=temp_draw_x_ind;
+								draw_y_ind+=num_width_height;
+							}
+						}
+						draw_y_ind=0;
+						draw_x_ind+=num_width_height*cnn_layers[i].matrix_width_height;
+					}
+					rect_x_ind+=num_width_height*cnn_layers[i].matrix_width_height*cnn_layers[i].matrix_values.length+10;
+					rect_y_ind=0;
+				}
+				else{
+					
+					grap.drawString("POOL", rect_x_ind, rect_y_ind+20);
+					rect_x_ind+=40;
+				}
+				
+			}	
+			//System.out.println("Done drawing");
+		}
+	}
+	
+	private class mouseevent implements MouseListener{
+		// Toggles whether the weights of a certain node should be shown. Done by clicking the node.
+		public void mouseClicked(MouseEvent e) {
+			// If the mouse was clicked on one of the nodes, toggle the drawweight property of the node and repaint
+			// the window to reflect the change
+			System.out.println(e.getPoint());
+		}
+		public void mouseEntered(MouseEvent e) {}
+		public void mouseExited(MouseEvent e) {}
+		public void mousePressed(MouseEvent e) {}
+		public void mouseReleased(MouseEvent e) {}
 	}
 }
--- src/cnn/cnn_testing.java
+++ src/cnn/cnn_testing.java
@@ -155,10 +155,13 @@ public class cnn_testing {
 		
 		
 		System.out.println("DONE");
-		*/
-		//please.print_values();
 		
+		
+		//please.print_values();
+		 * */
+		 
 		/*
+		
 		int data_depths[] = {3,2};
 		int data_width_heights[] = {5,3};
 		String layer_types[] = {"conv"};
@@ -167,19 +170,19 @@ public class cnn_testing {
 		cnn please = new cnn(data_depths,data_width_heights,layer_types,layer_depths,layer_width_heights);
 		
 		double input_data[][][] = {
-				{{2,2,1,0,2},{0,0,2,1,1},{0,2,1,0,0},{0,2,2,0,1},{1,0,2,0,1}},
-				{{2,0,2,2,2},{0,1,1,1,0},{2,2,0,2,0},{1,0,0,0,2},{2,2,0,2,1}},
-				{{1,1,0,2,2},{2,1,0,2,1},{1,1,1,2,1},{1,0,0,0,2},{0,2,1,0,1}}
+				{{2,2,1,0,2},{2,1,0,2,0},{2,0,1,2,0},{0,1,2,0,1},{2,2,1,1,2}},
+				{{0,1,2,1,1},{2,1,2,2,1},{1,1,1,0,0},{1,2,1,1,0},{1,1,2,0,2}},
+				{{2,1,1,2,1},{2,0,1,2,1},{1,2,1,2,2},{2,1,1,0,1},{1,0,1,1,0}}
 		};
 		
 		please.feedforward(input_data);
 		
 		//please.cnn_back_propagate();
-		please.print_values();
+		//please.print_values();
 		//please.print_weights();
 		 
 		*/
-		
+		/*
 		int data_depths[] = {3,2,2,1};
 		int data_width_heights[] = {5,3,2,2};
 		String layer_types[] = {"conv","pool","conv"};
@@ -188,9 +191,9 @@ public class cnn_testing {
 		cnn please = new cnn(data_depths,data_width_heights,layer_types,layer_depths,layer_width_heights);
 		
 		double input_data[][][] = {
-				{{2,2,1,0,2},{0,0,2,1,1},{0,2,1,0,0},{0,2,2,0,1},{1,0,2,0,1}},
-				{{2,0,2,2,2},{0,1,1,1,0},{2,2,0,2,0},{1,0,0,0,2},{2,2,0,2,1}},
-				{{1,1,0,2,2},{2,1,0,2,1},{1,1,1,2,1},{1,0,0,0,2},{0,2,1,0,1}}
+				{{2,2,1,0,2},{2,1,0,2,0},{2,0,1,2,0},{0,1,2,0,1},{2,2,1,1,2}},
+				{{0,1,2,1,1},{2,1,2,2,1},{1,1,1,0,0},{1,2,1,1,0},{1,1,2,0,2}},
+				{{2,1,1,2,1},{2,0,1,2,1},{1,2,1,2,2},{2,1,1,0,1},{1,0,1,1,0}}
 		};
 		
 		please.feedforward(input_data);
@@ -199,8 +202,40 @@ public class cnn_testing {
 		please.cnn_back_propagate(back_prop_error);
 		//please.print_values();
 		//please.print_weights();
-		please.print_weight_dev();
+		//please.print_weight_dev();
 		//please.print_xdev();
+		 */
+		int data_depths[] = {3,2,2,1};
+		int data_width_heights[] = {4,4,2,2};
+		String layer_types[] = {"conv","pool","conv"};
+		
+		int conv_constant_wid_hid = 3;
+		
+		int pool_constant_depth = 1;
+		int pool_constant_wid_hid = 2;
+		
+		int layer_depths[] = {2,pool_constant_depth,1};
+		int layer_width_heights[] = {conv_constant_wid_hid,pool_constant_wid_hid,conv_constant_wid_hid};
+		cnn please = new cnn(data_depths,data_width_heights,layer_types,layer_depths,layer_width_heights);
+		
+		double input_data[][][] = {
+				{{2,2,1,0},{2,1,0,2},{2,0,1,2},{0,1,2,0}},
+				{{0,1,2,1},{2,1,2,2},{1,1,1,0},{1,2,1,1}},
+				{{2,1,1,2},{2,0,1,2},{1,2,1,2},{2,1,1,0}}
+		};
+		
+		please.feedforward(input_data);
+		
+		double back_prop_error[] = {-1,2,4,0};
+		please.cnn_back_propagate(back_prop_error);
+		//please.print_values();
+		//please.print_weights();
+		//please.print_weight_dev();
+		//please.print_xdev();
+		
+		 
+		
+		
 	}
 	
 }
